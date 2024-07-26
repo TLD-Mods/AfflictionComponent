d800b047ce98cc7b57c0228070f2c14a52512ac7
Deadman <47807486+Deaadman@users.noreply.github.com>
2024-07-26 14:16:28 +0930

    Updated the status bars for buffs and debuffs now - works perfectly like vanilla

--- Patches/StatusBarPatches.cs
+++ Patches/StatusBarPatches.cs
@@ -14,7 +14,7 @@ internal static class StatusBarPatches
             var customAfflictions = AfflictionManager.GetAfflictionManagerInstance().m_Afflictions;
             foreach (var customAffliction in customAfflictions)
             {
-                __result = customAffliction.HasAfflictionRisk();
+                __result = customAffliction.HasAfflictionRisk() || !customAffliction.m_Buff;
             }
         }
     }
@@ -25,7 +25,11 @@ internal static class StatusBarPatches
         private static void Postfix(StatusBar __instance, ref bool __result)
         {
             if (__instance.m_StatusBarType != StatusBar.StatusBarType.Condition) return;
-            // Add logic here to determine whether the custom affliction is a buff and if it's active.
+            var customAfflictions = AfflictionManager.GetAfflictionManagerInstance().m_Afflictions;
+            foreach (var customAffliction in customAfflictions)
+            {
+                __result = customAffliction.m_Buff;
+            }
         }
     }
 }
