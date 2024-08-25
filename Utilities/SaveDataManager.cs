0beb5845d75034590644fa45c3423f50f7deeca9
Deadman <47807486+Deaadman@users.noreply.github.com>
2024-08-25 10:30:36 +0930

    updated header image

--- Utilities/SaveDataManager.cs
+++ Utilities/SaveDataManager.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ModData;
-using Il2Cpp;
-using AfflictionComponent.Components;
+﻿using AfflictionComponent.Components;
 using Newtonsoft.Json;
 
 namespace AfflictionComponent.Utilities
